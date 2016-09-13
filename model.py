c2eeb2497620b3dc94a788b6faaa69069b688dd3
Joshua Allen <jallen2@user001.palmetto.clemson.edu>
2016-09-13 17:54:58 -0400

    Added code to model.py to test later since jupyter hub was down.

--- model.py
+++ model.py
@@ -1,5 +1,22 @@
-list=[0]
 
-list[0]=1
+import xml.etree.ElementTree as et
+import glob 
 
-print(list[0])
+awards=dict()
+
+def addIDandAbs(ID,abstract):
+	for i in awards:
+		if i == ID:
+			print("Error")
+		return
+	awards[ID] = abstract
+
+for items in glob.glob("sample/0*.xml"):
+	item = open(items)                 		
+	str_data =  item.read()
+	data = et.fromstring(str_data)
+	award_list = data.findall("Award")
+for i in award_list
+	addIDandAbs(i.find("AwardID").text,i.find("AbstractNarration").text)
+	 
+print(awards)
