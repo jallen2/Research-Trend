fd5c93df79a168f23ecfe81728a31a408a8342cc
Joshua Allen <jallen2@user001.palmetto.clemson.edu>
2016-09-15 20:59:08 -0400

    Added 2002 xml files.

--- model.py
+++ model.py
@@ -2,21 +2,33 @@
 import xml.etree.ElementTree as et
 import glob 
 
+global awards
 awards=dict()
 
 def addIDandAbs(ID,abstract):
 	for i in awards:
 		if i == ID:
 			print("Error")
-		return
+			return
 	awards[ID] = abstract
-
-for items in glob.glob("sample/0*.xml"):
-	item = open(items)                 		
+    
+def addtolist():
+	for i in award_list:
+		addIDandAbs(i.find("AwardID").text,i.find("AbstractNarration").text)    
+#print(glob.glob("sample/0*.xml"))
+for items in glob.glob("sample/*.xml"):
+	item = open(items)
 	str_data =  item.read()
 	data = et.fromstring(str_data)
-	award_list = data.findall("Award")
-for i in award_list
-	addIDandAbs(i.find("AwardID").text,i.find("AbstractNarration").text)
-	 
-print(awards)
+#    global award_list 
+#    award_list = data.findall("Award")
+#    for i in award_list:
+#    addIDandAbs(i.find("AwardID").text,i.find("AbstractNarration").text)
+	addIDandAbs(data.find("Award").find("AwardID").text,data.find("Award").find("Institution").find("Name").text)
+	
+for k, v in awards.items(): 
+	sorted(awards.keys())
+	print(k+":", v)
+
+
+
